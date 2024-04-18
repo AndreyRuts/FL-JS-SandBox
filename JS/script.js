ae7152c7839646da717bdbe24f288f4748a455b8
Andrey Ruts <ravnb1989@gmail.com>
2024-04-18 15:54:12 +0300

    sb 1.3

--- JS/script.js
+++ JS/script.js
@@ -5,4 +5,25 @@ let user;
 user = 'Вася';
 let userName;
 userName = 'Вася';
-console.log(user)
+console.log(user);
+// --------------------------------
+let falseCheck = 9 > 10;
+console.log(falseCheck);
+console.log(typeof falseCheck);
+// =========================DataTypes=======================
+let userAge = 34;
+let userAgeInfo = `dik length = ${userAge}`;
+console.log(userAgeInfo);
+// -------------------------
+let changTypeTest = 'ТридцатьДва';
+console.log(changTypeTest);
+console.log(typeof changTypeTest);
+
+changTypeTest = Number(32);
+console.log(changTypeTest);
+console.log(typeof changTypeTest);
+// -------------------------
+let userHeight = "45" / "8";
+console.log(userHeight);
+console.log(typeof userHeight);
+// =========================Operators=======================
